89127a8127b51af2d846678058c7b333d3cacc20
mirador-cullender <daniel@mirador.org>
2026-02-02 14:01:36 +0000

    chore: cleanup

--- src/ingest/index.ts
+++ src/ingest/index.ts
@@ -10,9 +10,6 @@ export { Trace } from './trace';
 // Stack trace utilities
 export { captureStackTrace, formatStackTrace, formatStackTraceReadable } from './stacktrace';
 
-// Secret obfuscation utilities
-export { obfuscateSecrets } from './secrets';
-
 // Types
 export type {
   ClientOptions,
